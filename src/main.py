2bb9b9d43879c92e6b5f0646c8a096baf2373e67
vik.dev <83600218+vikdevelop@users.noreply.github.com>
2021-05-18 21:05:06 +0200

    Hlavní soubor

--- src/main.py
+++ src/main.py
@@ -16,9 +16,6 @@
 
 import sys
 import gi
-# Since a system can have multiple versions
-# of GTK + installed, we want to make 
-# sure that we are importing GTK + 3.
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gio
   
